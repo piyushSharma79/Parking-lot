b9a598ff58c3cb829643e6e3165660cde88e5d62
Piyush Sharma <ps1375982@gmail.com>
2026-06-09 11:24:47 +0530

    final commit

--- src/main/java/com/backend/parkinglot/ParkingLotRunner.java
+++ src/main/java/com/backend/parkinglot/ParkingLotRunner.java
@@ -69,6 +69,5 @@ public class ParkingLotRunner {
         requestDto.setGateId(gate1.getId());
 
         GenerateTicketResponseDto responseDto = ticketController.generateTicket(requestDto);
-        System.out.println("DEBUG");
     }
 }
